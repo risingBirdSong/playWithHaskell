0c11a4cf83c96f8add5cf59f2a16d4ecfdf5aebd
risingbirdsong <heregoingthere@outlook.com>
2020-09-17 15:30:26 -0700

    reimplemented maximum using recursion, satisfying

--- basicTypes.hs
+++ basicTypes.hs
@@ -109,3 +109,34 @@ guardPractice compare
     |  compare <= 30 = "between 10 and 30"
     |  compare <= 100 = "between 30 and 100"  
     |  otherwise = "over 100"
+
+max' a b
+    | a > b = a
+    | otherwise = b
+
+a `myCompare` b
+    | a > b = GT
+    | a == b = EQ
+    | a < b = LT
+
+
+-- example of let in bindings
+cylinder r h = 
+    let sideArea = 2 * pi * r * h  
+        topArea = pi * r ^2  
+    in  sideArea + 2 * topArea  
+
+  
+    -- let binding in list comprehension
+
+calcBmis_a xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi >= 25.0]  
+
+-- If let bindings are so cool, why not use them all the time instead of where bindings, you ask? Well, since let bindings are expressions and are fairly local in their scope, they can't be used across guards. Some people prefer where bindings because the names come after the function they're being used in. That way, the function body is closer to its name and type declaration and to some that's more readable.
+
+
+maximum' [] = error "error no list"
+maximum' [x] = x;
+maximum' (x:xs) 
+  | x > maxTail = x
+  | otherwise = maxTail
+  where maxTail = maximum' xs
--- readme.md
+++ readme.md
@@ -4,5 +4,3 @@ interesting answer here
 https://softwareengineering.stackexchange.com/questions/294983/why-do-haskell-and-scheme-use-singly-linked-lists/294984
 
 example of laziness, and the power of it.
-
-Part of a series of apps I made focusing on learning more about design, algorithms, React, TypeScript, CSS, UI and teaching through explanation and showing the process. Learning + fun + teaching.
