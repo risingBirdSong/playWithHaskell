eff756ebd267b7f0d7b4c8c91419540186f04837
risingbirdsong <heregoingthere@outlook.com>
2020-09-29 21:30:02 -0700

    first baby steps were to figure out how to write one mapping at a time, and then to compose them together

--- exercism/acronym.hs
+++ exercism/acronym.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Data.Text   (Text, split) 
+import Data.Text   (split) 
+import Data.Text as T
+
 
 testing = split (=='a') "aabbaca"
 
 -- and pointing out that the docs I was using were old. With the new docs I resolved the error. 
 
-note, i began getting this error but this stack overflow fixed the problem
+-- note, i began getting this error but this stack overflow fixed the problem
 
 -- https://stackoverflow.com/questions/37894987/couldnt-match-expected-type-text-with-actual-type-char
 
@@ -16,3 +18,12 @@ note, i began getting this error but this stack overflow fixed the problem
 
 -- https://mmhaskell.com/blog/2017/5/15/untangling-haskells-strings#:~:text=Haskell%20uses%205%20different%20types%20for%20representing%20strings.&text=The%20String%20type%20is%20a,representation%20most%20suited%20to%20serialization.
 -- OverloadedStrings compiler can make your life easier. It allows you to use a string literal refer to any of your different string types.
+
+message = pack "I am not angry. Not at all."
+tMap = T.map (\c -> if c == '.' then '!' else c) message
+-- mapTest = map head ["apple", "banana", "charlie"] -- "abc" , working test
+abbreviate xs =  Prelude.map T.head . split (==' ') $ xs
+
+mapToUpper = Prelude.map  T.toUpper  $  ["apple", "banana", "charlie"] -- "abc" , working test
+mapToHead = Prelude.map  T.head  $  ["apple", "banana", "charlie"] -- "abc" , working test
+mapBoth =  Prelude.map  T.head . Prelude.map T.toUpper  $  ["apple", "banana", "charlie"] -- "abc" , working test
