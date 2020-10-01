db076c6378e67d12bfa994dc6bc77d06881d81ae
risingbirdsong <heregoingthere@outlook.com>
2020-10-01 12:19:26 -0700

    why the negation in the definition of a typeclass, allows for minimal complete defintions

--- myTree.hs
+++ myTree.hs
@@ -15,6 +15,13 @@ treeInsert x (Node a left right)
 treeAddLeft = treeInsert 4 oneTree
 treeAddRight = treeInsert 6 oneTree
 
+treeElem :: (Ord a) => a -> Tree a -> Bool  
+treeElem x EmptyTree = False  
+treeElem x (Node a left right)  
+    | x == a = True  
+    | x < a  = treeElem x left  
+    | x > a  = treeElem x right 
+
 -- ok interesting so here is immutability in action, log out the three trees 
 -- oneTree Node 5 EmptyTree EmptyTree
 
@@ -25,3 +32,26 @@ treeAddRight = treeInsert 6 oneTree
 -- Node 5 EmptyTree (Node 6 EmptyTree EmptyTree)
 
 -- see they are totally distinct from each other!
+
+nums = [8,6,4,1,7,3,5]  
+numsTree = foldr treeInsert EmptyTree nums  
+-- (Node 3 (Node 1 EmptyTree EmptyTree) (Node 4 EmptyTree EmptyTree)) (Node 7 (Node 6 EmptyTree EmptyTree) (Node 8 EmptyTree EmptyTree))
+
+-- *Main> treeElem 8 numsTree  
+-- True
+-- *Main> treeElem 99 numsTree 
+-- False
+-- infix syntax
+-- *Main>  8 `treeElem` numsTree
+-- True
+
+-- weird and interest... type definition where each assertion is the negation of the opposite
+-- class Eq a where  
+--     (==) :: a -> a -> Bool  
+--     (/=) :: a -> a -> Bool  
+--     x == y = not (x /= y)  
+--     x /= y = not (x == y)  
+
+-- great and interesting answer to my previous observation 
+
+-- Because == was defined in terms of /= and vice versa in the class declaration, we only had to overwrite one of them in the instance declaration. That's called the minimal complete definition for the typeclass — the minimum of functions that we have to implement so that our type can behave like the class advertises. To fulfill the minimal complete definition for Eq, we have to overwrite either one of == or /=. If Eq was defined simply like this:
