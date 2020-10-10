c6d01425a9674c16e885813d6b4fb5603475c0bc
risingbirdsong <heregoingthere@outlook.com>
2020-10-10 12:01:35 -0700

    more primes, explanations, questions of memoization and sprinting

--- exercism/othersanagrams.hs
+++ exercism/othersanagrams.hs
@@ -107,3 +107,33 @@ listy = [ x | x <- [1..10]]
 filtered y = [ x | x <- [1..10], y + x < 10 ]
 
 comp_a_prime k = [ x | x <- [2..k - 1], k `mod` x == 0]
+
+doesItGoNegative = [5,4..] -- yes it does
+
+arrLengthCheck x = if length x > 1 then False else True
+
+isPrime k = if k > 1 then null [ x | x <- [2..k - 1], k `mod` x == 0] else if k < 1 then False else False
+
+primeList x = filter isPrime [x,x-1..1]
+
+-- great explanation by Melissa, thanks
+-- The expression is null [ x | x <- [2..k - 1], k `mod` x == 0].
+-- The list comprehension is generating the list of nontrivial divisors of k; if there are no nontrivial divisors, the list is empty, and null maps this to True.
+
+-- primes from Y Combinatorian
+primes :: [Integer]
+primes = sieve [2..]
+  where
+    sieve (p:ps) = p : sieve [x | x <- ps, x `mod` p /= 0]
+
+-- someNumbers = [1..100]
+-- someNumbers !! 5
+
+-- this one works but im finding sprint pretty strang and buggy to work with
+
+-- paste this GHCI
+-- someNumbers = [1..100] :: [Int]
+-- someNumbers !! 5
+-- :sprint someNumbers
+
+-- outputs someNumbers = 1 : 2 : 3 : 4 : 5 : 6 : _
---
+++ subsAndEquatReasoning.hs
@@ -0,0 +1,9 @@
+-- https://gilmi.me/blog/post/2020/10/01/substitution-and-equational-reasoning
+
+twentyOne =
+    let
+        seven = 7
+    in
+        seven + seven + 7
+
+fifteen = let five = 5 in five + five + five
