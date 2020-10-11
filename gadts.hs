681c7536f3fd73ee7a9a227c08af285a618017bb
risingbirdsong <heregoingthere@outlook.com>
2020-10-11 14:38:19 -0700

    this is where I get lost in the GADT types

--- gadts.hs
+++ gadts.hs
@@ -14,3 +14,22 @@ myAdd = Add (I 7) (I 7)
 fourteen = eval myAdd -- 14
 myMult = Mul (I 7) (I 7)
 fortyNine = eval myMult -- 49
+
+-- eval_b :: Expr_b -> Either Int Bool
+-- eval_b :: Expr_b -> Either Int Bool
+eval_b :: Expr_b -> Maybe (Either Int Bool)
+eval_b (I_b n)       = Expr_b n
+eval_b (B n)       =  Expr_b n 
+eval_b (Add_b e1 e2) = eval_b e1 + eval_b e2
+eval_b (Mul_b e1 e2) = eval_b e1 * eval_b e2
+data Expr_b = I_b Int
+          | B Bool           -- boolean constants
+          | Add_b Expr_b Expr_b
+          | Mul_b Expr_b Expr_b
+          | Eq  Expr_b Expr_b    -- equality test
+          deriving (Show, Eq, Ord)
+
+
+evaled_b1 = eval_b (I_b 2 `Add_b` I_b 4) -- 6
+-- the problem is the following type checks and even evaluates
+valid = B True `Add_b` I_b 3 :: Expr_b -- Add_b (B True) (I_b 3)
--- sorting.hs
+++ sorting.hs
@@ -12,3 +12,7 @@ quickSort_where (x:xs) = smallerSorted ++ [x] ++ biggerSorted
     smallerSorted = quickSort_where [a | a <- xs, a <= x]
     biggerSorted = quickSort_where [a | a <- xs, a > x]
 sorted_b = quickSort_where [9,1,8,2,7,3,6,4,5]
+
+expr = let x = 1 in 2 -- 2
+--I read let x = 1 in foo x as "let/allow  x to be 1 when evaluating the expression foo x"
+-- So the in is kind of what the function is itself, while the let part of the body is sort of the helper for building the in part?
