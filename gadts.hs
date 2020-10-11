892b2d0bb437ac638154f07b91f0367465b4c96c
risingbirdsong <heregoingthere@outlook.com>
2020-10-11 14:52:25 -0700

    getting in trouble

--- gadts.hs
+++ gadts.hs
@@ -4,11 +4,14 @@ data Expr = I Int
           | Mul Expr Expr
           | Eq  Expr Expr    -- equality test
 
-eval :: Expr -> Int
-eval (I n)       = n
+eval :: Expr -> Either Int Bool
+eval (I n) = Left n
+eval (B b) = Right b
 eval (Add e1 e2) = eval e1 + eval e2
 eval (Mul e1 e2) = eval e1 * eval e2
 
 -- this is a problem 
 -- eval (B b) = b is an error because the type signrature is returning an Int but this is return a Bool
 -- inference doesnt help
+
+-- but now we get in trouble with since we cant add booleans and the type checker complains
