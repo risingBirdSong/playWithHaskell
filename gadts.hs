4bd0b98f4322f8c70a248d12dac60a043c9d1095
risingbirdsong <heregoingthere@outlook.com>
2020-10-11 22:55:37 -0700

    great example of sum type and done for the night

--- gadts.hs
+++ gadts.hs
@@ -16,3 +16,16 @@ eval (Eq  e1 e2) = eval e1 == eval e2
 
 added = eval (I 3) + eval (I 3) -- 6
 added_a = (I 3) `Add` (I 4) -- 7
+
+data DivisionResult = DivisionByZero | Success Double
+    deriving (Show)
+
+safeDivide :: Double -> Double -> DivisionResult
+safeDivide x y =
+    if y == 0
+    then DivisionByZero
+    else Success (x / y)
+
+-- Try dividing by zero
+safe = safeDivide 9 3 -- Success 3.0
+notSafe = safeDivide 11 0 -- DivisionByZero
