5efc5ecb847308ffe2a25429e21a90fc080bf7eb
risingbirdsong <heregoingthere@outlook.com>
2020-10-12 22:51:59 -0700

    saw a nice solution after I solved, but honestly still dont understand it how its accumulating

--- codeWars/binary.hs
+++ codeWars/binary.hs
@@ -1,8 +1,8 @@
 import Data.Binary
 import GHC.Float
 import GHC.Float.RealFracMethods
+import Data.List
 -- gotA = get 
-  
 convert :: Double -> Int
 convert x = round x
 
@@ -10,3 +10,8 @@ binMult :: [Int]
 binMult = (map convert (map (2**) [0,1..]))
 
 toNumber xs =sum ((zipWith (*) binMult (reverse xs)))
+
+toNumber' :: [Int] -> Int
+toNumber' = foldl f 0
+  where f acc n = 2*acc+n
+
