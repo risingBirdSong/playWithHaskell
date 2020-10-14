abf82f02f7383c8e9685d1cc61e023e5e8fb70f0
risingbirdsong <heregoingthere@outlook.com>
2020-10-14 16:28:38 -0700

    an example of the issue and a takeway, its good to write your type signatures, the inference is generally good but not always correct!

--- monomorphismrestriction.hs
+++ monomorphismrestriction.hs
@@ -1,9 +1,10 @@
-
+-- https://wiki.haskell.org/Monomorphism_restriction
 -- A simple example is plus = (+). Without an explicit signature for plus, 
 -- the compiler will not infer the type (+) :: (Num a) => a -> a -> a for `plus`, 
 -- but will apply defaulting rules to specify plus :: Integer -> Integer -> Integer. 
 
 -- and yes sure enough 
+import Data.List
 plus = (+)
 
 -- here the inferred type doesnt work as expected but adding the more generic type does
@@ -12,3 +13,18 @@ typedPlus = (+)
 
 -- To a first approximation it means that you often cannot overload a function 
 -- unless you provide an explicit type signature.
+
+f xs = (len,len)
+     where
+       len = genericLength xs
+
+-- The type of f, if no signature is given, then the compiler doesn't know that the two elements of the returned
+-- pair are of the same type. It's return value will be:
+-- f::(Num a, Num b) => [x] -> (a, b)
+
+-- why not
+-- Num a => [x] -> (a, a)
+
+-- the article then goes into more details and examples that are currently difficult for me to follow...
+-- So for now the takeaway is that it's very handy to type your own signatures and if an inferred type 
+-- seems off, then monomorphism restriction is a good thing to consider as the problem
