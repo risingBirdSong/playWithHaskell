3e88df5b8fce738c5be9d1efebba625d86f8ca52
risingbirdsong <heregoingthere@outlook.com>
2020-10-15 15:25:26 -0700

    an answer to my question of how to call flatten on an arbitrarily nested amount of values, without the use of custom data type One and Many. CT Crumples points out this can't be done, for standard list the nesting must be uniform

--- minichallenges.hs
+++ minichallenges.hs
@@ -16,17 +16,21 @@ concatAnyNest [] = []
 concatAnyNest xs = concat xs
 
 data Item a = One a | Many [Item a]
-
 flatten (One x) = [x]
 flatten (Many xs) = concat $ map flatten xs
+ex3 = flatten (Many [(One 1), Many [(One 2), Many [One 3, One 4]]]) -- [1,2,3,4]
 
 -- infinite type
 -- flatten'  x = [x]
 -- flatten'  xs = concat $ map flatten xs
 firstWorking =  flatten (One 4) -- [4]
 ex2 = flatten (Many [(One 3), (One 4)]) -- [3,4]
-ex3 = flatten (Many [(One 1), Many [(One 2), Many [One 3, One 4]]]) -- [1,2,3,4]
 
+-- The default list is homogeneous, so every value in it must have the same type.
+-- If some values are Int and some values are [Int] then those things aren't of the same type
+
+-- You can have a list of Ints, a list of lists of Ints, 
+-- a list of lists of lists of Ints... but the level of nesting must be uniform
 
 buildNode x = if 2*x + 1 > 7 then (x, []) else (x, [2*x, 2*x+1])
 makeAndShow = putStr $ T.drawTree $ fmap show $ T.unfoldTree buildNode 1
@@ -48,8 +52,10 @@ makeAndShow = putStr $ T.drawTree $ fmap show $ T.unfoldTree buildNode 1
 lrgst [] = Nothing
 lrgst xs = Just (head (reverse $ sort xs))
 
+maxed :: (Foldable t, Ord b) => b -> t b -> b
 maxed strt xs = foldr max strt xs
 
+max' :: Ord a => [a] -> a -> a
 max' [] c = c 
 max' (x:xs) c
   | x >= c = max' xs x
