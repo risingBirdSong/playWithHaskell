0b16e065c54e8ea08bf8ad4a036db8e0b7e28362
risingbirdsong <heregoingthere@outlook.com>
2020-10-14 18:30:09 -0700

    learned more about the way this is called and used with the One and Many types, but I curious if this func can be made without the extra types used by the caller

--- minichallenges.hs
+++ minichallenges.hs
@@ -1,10 +1,27 @@
 
 -- see if list of strings contain given chars
-  filtering xs c = filter (==c) xs -- "oo"
+filtering xs c = filter (==c) xs -- "oo"
 
-  contains y xs = any (==y) xs
-  
-  listContains xxs y = map (contains y) xxs
-  
-  -- add list of list numbers
-  add' xs = sum (map (sum) xs)
+contains y xs = any (==y) xs
+
+listContains xxs y = map (contains y) xxs
+
+-- add list of list numbers
+add' xs = sum (map (sum) xs)
+
+-- does concat flatten any amount of nesting?
+
+concatAnyNest [] = [] 
+concatAnyNest xs = concat xs
+
+data Item a = One a | Many [Item a]
+-- flatten :: Item a -> [a]
+flatten (One x) = [x]
+flatten (Many xs) = concat $ map flatten xs
+
+-- infinite type
+-- flatten'  x = [x]
+-- flatten'  xs = concat $ map flatten xs
+firstWorking =  flatten (One 4) -- [4]
+ex2 = flatten (Many [(One 3), (One 4)]) -- [3,4]
+ex3 = flatten (Many [(One 1), Many [(One 2), Many [One 3, One 4]]]) -- [1,2,3,4]
